7751c295155e3f89d270633a5ce2fb9baf03762f
Oleh Kozlovskyi <okozlovskyi@n-ix.com>
2019-12-02 14:59:43 +0200

    add bootcamps router and controller

---
+++ controllers/bootcamps.js
@@ -0,0 +1,19 @@
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ data: { id: 10 } });
+};
+
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ data: { id: 10 } });
+};
+
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ data: { id: 10 } });
+};
+
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({ data: { id: 10 } });
+};
+
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({ data: { id: 10 } });
+};
