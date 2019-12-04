08eebfac68dd9d9215c12d9d1defdce9aced2e02
Oleh Kozlovskyi <okozlovskyi@n-ix.com>
2019-12-04 13:38:30 +0200

    create Bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 exports.getBootcamps = (req, res, next) => {
   res.status(200).json({ data: { id: 10 } });
 };
@@ -6,8 +8,19 @@ exports.getBootcamp = (req, res, next) => {
   res.status(200).json({ data: { id: 10 } });
 };
 
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ data: { id: 10 } });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (e) {
+    res.status(400).json({
+      success: fasle,
+    });
+  }
 };
 
 exports.updateBootcamp = (req, res, next) => {
