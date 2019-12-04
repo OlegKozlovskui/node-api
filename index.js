ea42df8098be575f905bc6f061a56852434ba64b
Oleh Kozlovskyi <okozlovskyi@n-ix.com>
2019-12-04 12:19:46 +0200

    connect to DB

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
+// Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -19,4 +25,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => promise.exit(1));
+});
