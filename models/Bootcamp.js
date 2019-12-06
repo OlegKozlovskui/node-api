c7cd0d6497d6dc4c862e11a2fc3d3a087eb31007
Oleh Kozlovskyi <okozlovskyi@n-ix.com>
2019-12-06 11:39:37 +0200

    add Bootcamp slug

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -98,6 +99,10 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   },
+});
+
+BootcampSchema.pre('save', function() {
+  this.slug = slugify(this.name, { lower: true });
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
