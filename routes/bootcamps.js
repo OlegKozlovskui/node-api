b2ee12f72ece464631fad0f8c891a6d02fc0aa37
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2019-12-07 14:12:10 +0200

    add Course router

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const courseRouter = require('./courses');
+
+router.use('/:bootcampId/courses', courseRouter);
+
 const {
   getBootcamps,
   getBootcamp,
