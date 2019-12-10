fc05ef502a11b4b3009b84cc3ae3e27015db5a4a
Oleh Kozlovskyi <okozlovskyi@n-ix.com>
2019-12-10 15:09:23 +0200

    implement register flow

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const connectDB = require('./config/db');
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
