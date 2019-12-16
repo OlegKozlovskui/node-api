21c007cd60c0c7ec7520bcbf8a9d39ae7514ff04
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2019-12-16 11:59:48 +0200

    prevent xss

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const morgan = require('morgan');
 const colors = require('colors');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
-const mongoSanitaze = require('express-mongo-sanitize');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
@@ -39,8 +41,14 @@ if (process.env.NODE_ENV === 'development') {
 // File uploading
 app.use(fileUpload());
 
-// Sanitaze data
-app.use(mongoSanitaze());
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
